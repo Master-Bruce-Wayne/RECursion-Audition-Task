70c9c5d2ac921602691310d22fa6b9baf3bab8e7
Sujal Agarwal <sujalagarwal.nitdgp@gmail.com>
2025-11-09 15:20:29 +0530

    Backend socket started

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ import { Server } from 'socket.io';
 import userRouter from './routes/userRoute.js'
 import gameRouter from './routes/gameRoute.js'
 import leaderBoardRouter from './routes/leaderBoardRoute.js';
-// import { setupSocketIO } from './socket/socketHandler.js';
+import { setupSocketIO } from './socket/socketHandler.js';
 
 dotenv.config({});
 
@@ -20,14 +20,7 @@ const PORT = process.env.PORT || 3000;
 // Create HTTP server
 const httpServer = createServer(app);
 
-// Initialize Socket.IO
-const io = new Server(httpServer, {
-    cors: {
-        origin: process.env.FRONTEND_URL || "http://localhost:5173",
-        credentials: true,
-        methods: ["GET", "POST"]
-    }
-});
+setupSocketIO(httpServer);
 
 app.use(cors({
     // origin: process.env.FRONTEND_URL,
@@ -45,9 +38,6 @@ app.use('/api/v1/stats', leaderBoardRouter);
 app.get('/', (req,res) => {
     return res.send("Kaam kar raha hai");
 });
-
-// Setup Socket.IO handlers
-// setupSocketIO(io);
 
 // connect db first then start server
 const startServer = async () => {
--- backend/socket/socketHandler.js
+++ backend/socket/socketHandler.js
@@ -1,12 +1,45 @@
-// import {io} from 
+import { Server } from 'socket.io';
 
-export const setupSocketIO = (io) => {
+export const setupSocketIO = (server) => {
+    const io = new Server(server, {
+        cors: {
+            origin: process.env.FRONTEND_URL || "http://localhost:5173",
+            credentials: true,
+            methods: ["GET", "POST"]
+        },
+    });
+
     io.on("connection", (socket) => {
         console.log(`User connected: ${socket.id}`);
 
-        // Join a game room
-        socket.on('join_game', ({ roomId }) => {
-            socket.join(roomId);
-        })
-    })
-}
+        socket.on("join_room", (roomCode) => {
+            socket.join(roomCode);
+            console.log(` User ${socket.id} joined room: ${roomCode}`);
+            io.to(roomCode).emit("player_joined", { playerId: socket.id });
+        });
+
+        socket.on("player_move", (data) => {
+            // data = { roomCode, moveData }
+            console.log(`Move received in ${data.roomCode}:`, data.moveData);
+            io.to(data.roomCode).emit("receive_move", data);
+        });
+
+        socket.on("send_message", (data) => {
+            // data = { roomCode, message, player }
+            io.to(data.roomCode).emit("receive_message", data);
+        });
+
+        socket.on("game_over", (data) => {
+            // data = { roomCode, winner, stats }
+            io.to(data.roomCode).emit("game_result", data);
+        });
+
+        socket.on("disconnect", () => {
+            console.log("Client disconnected:", socket.id);
+        });
+    });
+
+    console.log("Socket.IO server is running and initialised");
+    return io;
+};
+
